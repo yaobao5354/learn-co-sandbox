d40c1291e3e2e5935a213a84a7d3e9c9e71a704a
yaoyu bao <yaobao5354@gmail.com>
2020-06-09 03:23:17 +0000

    Automatically backed up by Learn

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1 , "Tabby", 1);
+INSERT INTO cats (name, age, breed) VALUES ("Lil\' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
